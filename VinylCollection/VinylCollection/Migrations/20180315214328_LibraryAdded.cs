f7bd9b894125b60a9babc5759ef2dd6c2797a02f
Joshua Donegal <32005113+hyperumbra@users.noreply.github.com>
2018-03-16 10:12:25 -0400

    Add files via upload

---
+++ VinylCollection/VinylCollection/Migrations/20180315214328_LibraryAdded.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace VinylCollection.Migrations
+{
+    public partial class LibraryAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Library",
+                columns: table => new
+                {
+                    CollectorID = table.Column<Guid>(nullable: false),
+                    Age = table.Column<int>(nullable: false),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Library", x => x.CollectorID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Record",
+                columns: table => new
+                {
+                    CollectorID = table.Column<Guid>(nullable: false),
+                    LibraryCollectorID = table.Column<Guid>(nullable: true),
+                    Rating = table.Column<int>(nullable: false),
+                    Title = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Record", x => x.CollectorID);
+                    table.ForeignKey(
+                        name: "FK_Record_Library_LibraryCollectorID",
+                        column: x => x.LibraryCollectorID,
+                        principalTable: "Library",
+                        principalColumn: "CollectorID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Record_LibraryCollectorID",
+                table: "Record",
+                column: "LibraryCollectorID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Record");
+
+            migrationBuilder.DropTable(
+                name: "Library");
+        }
+    }
+}
---
+++ VinylCollection/VinylCollection/Models/Library.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VinylCollection.Models
+{
+    public class Library
+    {
+        [Key]
+        public Guid CollectorID { get; set; }
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public int Age { get; set; }
+
+        public virtual Collection<Record> RecordList { get; set; }
+
+        public Library()
+        {
+            RecordList = new Collection<Record>();
+        }
+
+
+    }
+}
